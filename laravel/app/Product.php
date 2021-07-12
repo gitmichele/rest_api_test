a538e5c81bb4599c759e8a1e8fbb47ab7c533895
michele <micheleepedrotti@gmail.com>
2021-07-12 17:15:06 +0200

    DONE: setup models (product - review)

--- laravel/app/Product.php
+++ laravel/app/Product.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Review;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'detail', 'stock','price','discount'
+    ];
+
+    public function reviews()
+    {
+        return $this ->hasMany(Review::class);
+    }
 }
--- laravel/app/Review.php
+++ laravel/app/Review.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Product;
 
 class Review extends Model
 {
-    //
+    protected $fillable = [
+        'customer', 'star', 'review',
+    ];
+
+    public function product()
+    {
+        return $this ->belongsTo(Product::class);
+    }
 }
