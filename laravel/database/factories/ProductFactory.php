a91700f36e3bcbc1bd6ac880dd9dd43d1d0e394f
michele <micheleepedrotti@gmail.com>
2021-07-12 17:24:38 +0200

    DONE: populate DB with fake data

--- laravel/database/factories/ProductFactory.php
+++ laravel/database/factories/ProductFactory.php
@@ -7,6 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->word(),
+        "detail" => $faker->paragraph(),
+        "price" => $faker->numberBetween(100,1000),
+        "stock" => $faker->randomDigit(),
+        "discount" => $faker->numberBetween(2,30),
+        "user_id" => function(){
+        	return \App\User::all()->random();
+        }
     ];
 });
